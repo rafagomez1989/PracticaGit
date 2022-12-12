aae75faef3c4d8f8a7493f808e6719b8bc79213b
rafag <rafagomezcruz89@gmail.com>
2022-12-12 14:23:49 +0100

    Declaración de variables y petición de dos números

---
+++ operaciones.java
@@ -0,0 +1,24 @@
+package operaciones;
+
+import java.util.Scanner;
+
+import javax.swing.JOptionPane;
+
+public class operaciones {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+
+		String str;
+		int opcion, num1, num2; 
+		
+		System.out.println("Introduzca un número entero: ");
+		num1 = sc.nextInt();
+
+		System.out.println("Introduzca otro número entero: ");		
+		num2 = sc.nextInt();
+			
+
+	}
+
+}
